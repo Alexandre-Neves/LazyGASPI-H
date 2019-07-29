ff31ded7994d75783bb044bea30fd5263aef8920
Alexandre-Neves <alexandre.neves@tecnico.ulisboa.pt>
2019-07-29 17:13:44 +0100

    Removed notification. Now waits until request is fulfilled

--- src/write.cpp
+++ src/write.cpp
@@ -12,16 +12,18 @@ gaspi_return_t lazygaspi_write(lazygaspi_id_t row_id, lazygaspi_id_t table_id, v
     auto data = (LazyGaspiRowData*)((char*)info->rows + offset);
     data->age = info->age;
 
-    PRINT_DEBUG("Writing row of age " << data->age);
+    PRINT_DEBUG_INTERNAL("Writing row " << row_id << " of table " << table_id << " of age " << data->age
+                         << " at offset " << offset);
 
     memcpy((char*)(info->rows) + offset + sizeof(LazyGaspiRowData), row, info->row_size);
 
     for(gaspi_rank_t rank = 0; rank < info->n; rank++) if(rank != info->id){
-        r = writenotify(SEGMENT_ID_ROWS, SEGMENT_ID_ROWS, offset, offset, sizeof(LazyGaspiRowData) + info->row_size, rank,
-                    NOTIF_ID_ROW_WRITTEN);
+        r = writecopy(SEGMENT_ID_ROWS, offset, sizeof(LazyGaspiRowData) + info->row_size, rank);
         ERROR_CHECK;
-        PRINT_DEBUG("Wrote to rank " << rank);
+        PRINT_DEBUG_INTERNAL("Wrote to rank " << rank);
     }
+    //Ensures data is sent before it is overwritten by another call to this function
+    r = gaspi_wait(0, GASPI_BLOCK); ERROR_CHECK;
 
     return GASPI_SUCCESS;
 }
