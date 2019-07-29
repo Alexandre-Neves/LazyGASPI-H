fdb4841b324031db6f4dada0fb7f668bcbcc7c06
Alexandre-Neves <alexandre.neves@tecnico.ulisboa.pt>
2019-07-29 17:10:21 +0100

    Added scripts

--- run_app.sh
+++ run_app.sh
@@ -1,1 +1,12 @@
-../GPI/bin/gaspi_run -m machinefile ${PWD}/bin/test.o
+rm -f lazygaspi_h_*.out
+rm -f ~/lazygaspi_h_*.out
+ssh alexandrepc "rm -f ~/lazygaspi_h_*.out"
+
+args="-k 6 -n 5 -r 7 -i 10"
+if [[ $# > 0 ]]; then
+    args=$@
+fi
+echo "../GPI/bin/gaspi_run -m machinefile ${PWD}/bin/test.o $args"
+../GPI/bin/gaspi_run -m machinefile ${PWD}/bin/test.o $args
+    
+
