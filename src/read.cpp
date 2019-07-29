9c73519241ae5f7e498eee3701cc37631e4718c3
Alexandre-Neves <alexandre.neves@tecnico.ulisboa.pt>
2019-07-29 17:13:22 +0100

    Removed wait for notication

--- src/read.cpp
+++ src/read.cpp
@@ -4,8 +4,6 @@
 
 #include <cstring>
 
-#define WAIT_TIMEOUT 100
-
 gaspi_return_t lazygaspi_read(lazygaspi_id_t row_id, lazygaspi_id_t table_id, lazygaspi_slack_t slack, void* row, 
                               LazyGaspiRowData* outData){
     LazyGaspiProcessInfo* info;
@@ -13,19 +11,16 @@ gaspi_return_t lazygaspi_read(lazygaspi_id_t row_id, lazygaspi_id_t table_id, la
 
     auto data = (LazyGaspiRowData*)((char*)info->rows + get_row_offset(info, row_id, table_id));
     Notification notif;
-    const auto min = get_min_age(info->age, slack);
+    const auto min = get_min_age(info->age, slack, info->offset_slack);
 
-    PRINT_DEBUG("Reading row (" << row_id << ", " << table_id << ") with minimum age " << min);
+    PRINT_DEBUG_INTERNAL("Reading row (" << row_id << ", " << table_id << ") with minimum age " << min);
+    #if defined DEBUG || defined DEBUG_INTERNAL
+    if(data->age < min) PRINT_DEBUG_INTERNAL("Age below minimum (" << data->age << "). Waiting for write...");
+    #endif
 
-    while(data->age < min){
-        PRINT_DEBUG("Age below minimum. Waiting for notification...");
-        
-        r = get_notification(SEGMENT_ID_ROWS, NOTIF_ID_ROW_WRITTEN, 1, &notif, WAIT_TIMEOUT); 
-        if(r == GASPI_TIMEOUT) continue;
-        ERROR_CHECK;
-    }
+    while(data->age < min);
 
-    PRINT_DEBUG("Age fresh enough. Outputting row...");
+    PRINT_DEBUG_INTERNAL("Age fresh enough (" << data->age << "). Outputting row...");
 
     memcpy(row, (char*)data + sizeof(LazyGaspiRowData), info->row_size);
     if(outData) *outData = *data;
