81a531de33b6d2e369d4946983c958ac18ea9f50
Alexandre-Neves <alexandre.neves@tecnico.ulisboa.pt>
2019-08-01 12:25:40 +0100

    New scripts

--- run_app.sh
+++ run_app.sh
@@ -2,7 +2,7 @@ rm -f lazygaspi_h_*.out
 rm -f ~/lazygaspi_h_*.out
 ssh alexandrepc "rm -f ~/lazygaspi_h_*.out"
 
-args="-k 6 -n 5 -r 7 -i 10"
+args="-k 4 -n 2 -r 6"
 if [[ $# > 0 ]]; then
     args=$@
 fi
